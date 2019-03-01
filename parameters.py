e9eb2e5aeffcc2f45ebbbb5cbf5f18cd4bb30cbe
rajat <rajat@rajats-MacBook-Pro.local>
2019-03-01 08:22:24 +0530

    updated

--- parameters.py
+++ parameters.py
@@ -1,4 +1,4 @@
 VOCAB_FILE = 'weights/pubmed_pmc_470k/vocab.txt'
 BERT_CONFIG_FILE = 'weights/pubmed_pmc_470k/bert_config.json'
-BC5CDR_WEIGHT = 'weights/bc5cdr.pt'
-BIONLP13CG_WEIGHT = 'weights/bionlp13cg.pt'
+BC5CDR_WEIGHT = 'weights/bc5cdr_wt.pt'
+BIONLP13CG_WEIGHT = 'weights/bionlp13cg_wt.pt'
