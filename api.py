417ae20c0872b48fc3ca3fe0a01780e7211fa4e8
Deepanshu <deepanshu2017@gmail.com>
2019-03-01 13:29:39 +0530

    Added starlette app
    Signed-off-by: Deepanshu <deepanshu2017@gmail.com>

--- api.py
+++ api.py
@@ -3,12 +3,18 @@ from new_model import Net
 from pytorch_pretrained_bert.modeling import BertConfig
 from pytorch_pretrained_bert import BertModel
 import parameters
-from flask import Flask
-from flask import request, abort, jsonify
-import torch 
 import numpy as np 
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse, HTMLResponse, RedirectResponse
+import torch
+import sys
+import uvicorn
+import aiohttp
+
 
 config = BertConfig(vocab_size_or_config_json_file=parameters.BERT_CONFIG_FILE)
+app = Starlette()
+
 
 def build_model(config, state_dict, hp):
     model = Net(config, vocab_len = len(hp.VOCAB), bert_state_dict=None)
@@ -40,7 +46,7 @@ def process_query(query, hp, model):
 
     # Process query 
     model.eval()
-    _, _, y_pred = model(x, torch.Tensor([1,2,3]))  # just a dummy y value 
+    _, _, y_pred = model(x, torch.Tensor([1, 2, 3]))  # just a dummy y value
     preds = y_pred[0].cpu().numpy()[np.array(is_heads) == 1]  # Get prediction where head is 1 
 
     # convert to real tags and remove <SEP> and <CLS>  tokens labels 
@@ -51,25 +57,49 @@ def process_query(query, hp, model):
     return final_output
 
 
-app = Flask(__name__)
-@app.route('/api/ner/bc5cdr/', methods = ['GET', 'POST'])
-def get_bc5cdr():
-    print(request.headers, request.is_json)
-    query = request.values['query']
+def get_bc5cdr(query):
     hp = HParams('bc5cdr')
     out = process_query(query=query, hp=hp, model=bc5_model)
-    return jsonify({'tagging': out})
+    return JSONResponse({'tagging': out})
 
 
-
-@app.route('/api/ner/bionlp13cg/', methods = ['GET', 'POST'])
-def get_bionlp13cg():
-    print(request.headers, request.is_json)
-    query = request.values['query']
+def get_bionlp13cg(query):
     hp = HParams('bionlp3g')
     out = process_query(query=query, hp=hp, model=bionlp13cg_model)
-    return jsonify({'tags': out})
+    return JSONResponse({'tags': out})
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='9000')
+@app.route("/extract-ner", methods=["GET"])
+async def extract_ner(request):
+    text = request.query_params["text"]
+    if "bionlp3g" in request.query_params:
+        return get_bionlp13cg(text)
+    else:
+        return get_bc5cdr(text)
+
+
+@app.route("/")
+def form(_):
+    return HTMLResponse(
+        """
+        <h3>This app will find the NER!<h3>
+        <form action="/extract-ner" method="get">
+            <textarea rows="10" cols="60" name="text">
+            </textarea><br>
+            <input type="submit" name="bionlp3g" value="BIO NLP 3G">
+            <input type="submit" name="bc5cdr" value="BC 5 CDR">
+        </form>
+    """)
+
+
+@app.route("/form")
+def redirect_to_homepage(_):
+    return RedirectResponse("/")
+
+
+if __name__ == "__main__":
+    # To run this app start application on server with python
+    # python FILENAME serve
+    # ex: python server.py server
+    if "serve" in sys.argv:
+        uvicorn.run(app, host="0.0.0.0", port=9000)
