5271116811e919a930241910b42d0627c4f1ad27
Subroto <subroto@bangthetable.com>
2019-03-09 12:47:52 +0530

    Updates

---
+++ precache-manifest.70ca826d4a312e578caaa8978f1f81ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8c6abd2f28eed834f30",
+    "url": "/static/css/main.9c482c5a.chunk.css"
+  },
+  {
+    "revision": "d8c6abd2f28eed834f30",
+    "url": "/static/js/main.d8c6abd2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a02764a714b7d37857de",
+    "url": "/static/js/2.a02764a7.chunk.js"
+  },
+  {
+    "revision": "9ed1d22c4da4110bbb2d91eb49db64a3",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/SolvingAlmostAnythingWithBert/precache-manifest.6e29ba42436412d796169362a79fcde6.js"
+  "/precache-manifest.70ca826d4a312e578caaa8978f1f81ae.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/SolvingAlmostAnythingWithBert/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
